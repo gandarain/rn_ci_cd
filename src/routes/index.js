43dab1e29a68ee1fa08ab2d7cba5f076b4a37ae8
Ganda Rain Panjaitan <ganda@halojasa.id>
2021-02-23 17:22:56 +0700

    setup screen

---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Icon} from 'native-base';
+import HomeScreen from '../views/homeScreen';
+import ProfileScreen from '../views/profileScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'account' : 'account';
+            }
+            return (
+              <Icon
+                type="MaterialCommunityIcons"
+                name={iconName}
+                style={{color: color, fontSize: size}}
+              />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
